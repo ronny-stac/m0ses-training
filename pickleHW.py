62812023dc057cdd0ceed9256daa63f7b2c74acb
omondi-paul <omondipaulok@gmail.com>
2024-07-25 23:04:59 +0300

    master

---
+++ pickleHW.py
@@ -0,0 +1,21 @@
+import pickle
+names=[]
+grades=[]
+numStudents=int(input('How Grades Do You Have? '))
+for j in range(0,numStudents,1):
+  name=input('Please Enter Student Name: ')
+  names.append(name)
+  prompt='Please Enter'+name+"'s grade'"
+  grade=float(input(prompt))
+  grades.append(grade)
+with open('studentData.pkl','wb') as dataF:
+  pickle.dump(numStudents,dataF)  
+  pickle.dump(names,dataF) 
+  pickle.dump(grades,dataF)
+with open('studentData.pkl','rb') as readF:
+  a=pickle.load(readF)
+  b=pickle.load(readF)
+  c=pickle.load(readF)    
+print(a)
+print(b)
+print(c)           
