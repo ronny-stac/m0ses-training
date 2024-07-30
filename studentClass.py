622bc31d6293b5b9c85e52c272edbcaf935b358e
omondi-paul <omondipaulok@gmail.com>
2024-07-30 13:05:52 +0300

    third commit

--- studentClass.py
+++ studentClass.py
@@ -5,16 +5,45 @@ class Students:
   def gInput(self,ng):
     self.ng=ng
     self.grades=[]
-    for i in range(0,ng,1):
-      grd=float(input('Please Enter Your Grade: '))
+    for i in range(0,self.ng,1): 
+      grd=float(input('Please Enter '+self.first +"'s Grade: "))
       self.grades.append(grd)
     return self.grades
-
+  def printGrades(self):
+    print(self.first,self.last+"'s Grades are: ")
+    for i in range(0,self.ng,1):
+      print(self.grades[i])
+    print('')  
+  def avGrades(self):
+    bucket=0
+    for i in range(0,self.ng,1):
+      bucket=bucket+self.grades[i]
+    avg=bucket/self.ng
+    return avg   
+  def highlow(self):
+    highGrade=0
+    lowGrade=100
+    for i in range(0,self.ng,1):
+      if self.grades[i]>highGrade:
+        highGrade=self.grades[i]
+      if self.grades[i]<lowGrade:
+        lowGrade=self.grades[i]
+    return lowGrade,highGrade    
 student1=Students('Joe','Evans')
+student1.gInput(4)
 student2=Students('shirly','Baker')
+student2.gInput(6)
 
-print(student1.first,student1.last)    
-print(student2.first,student2.last) 
+print('Grade Report for ',student1.first,student1.last)
+student1.printGrades()
+print('Average is: ',student1.avGrades())
+lowGrade,highGrade=student1.highlow()
+print('Low Grade is: ',lowGrade)
+print('High Grade is: ',highGrade)
 
-testGrades=student1.gInput(4)
-print(testGrades)
+print('Grade Report for ',student2.first,student2.last)
+student2.printGrades()
+print('Average is: ',student2.avGrades())
+lowGrade,highGrade=student2.highlow()
+print('Low Grade is: ',lowGrade)
+print('High Grade is: ',highGrade)
